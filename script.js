adf4f7ac637b548face40e534a148538471ad8df
Hetoreno <hectortamriel@gmail.com>
2020-07-10 19:40:47 -0700

    adding and updating files

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const buttton = document.querySelector(`.submit`);
 const list = document.querySelector(`#todo-list`);
 
 buttton.addEventListener("click",function (){
-    const newItem = document.createElement("li")
+    const newItem = document.createElement("li");
+    const cButton = document.createElement("button");
 
     newItem.classList.add(`item`);
     newItem.innerText = input.value;
     input.value="";
     list.appendChild(newItem);
+
+    cButton.classList.add(`check`);
+    cButton.innerText = "sd";
+    list.appendChild(cButton);
+
+    
 });
